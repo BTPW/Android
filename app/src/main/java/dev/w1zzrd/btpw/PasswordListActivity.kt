d093dbec77b4544ac4aac1d3385abeb1aaf127c2
Gabriel Tofvesson <gabto095@student.liu.se>
2022-01-08 17:04:59 +0100

    BA-6 Add password list activity

---
+++ app/src/main/java/dev/w1zzrd/btpw/PasswordListActivity.kt
@@ -0,0 +1,12 @@
+package dev.w1zzrd.btpw
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PasswordListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_passwordlist)
+    }
+}
