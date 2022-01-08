898b5e13083ada8aa38bd4242e7ab4a82bd6a634
Gabriel Tofvesson <gabto095@student.liu.se>
2022-01-08 16:07:34 +0100

    BA-0 Initial commit

---
+++ app/src/androidTest/java/dev/w1zzrd/btpw/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package dev.w1zzrd.btpw
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("dev.w1zzrd.btpw", appContext.packageName)
+    }
+}
