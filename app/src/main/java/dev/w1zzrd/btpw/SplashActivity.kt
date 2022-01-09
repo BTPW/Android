333fff9961140decaa2c607cd53ebead27b2d0b4
Gabriel Tofvesson <gabto095@student.liu.se>
2022-01-09 03:24:50 +0100

    BA-2 Adjust styles for application

--- app/src/main/java/dev/w1zzrd/btpw/SplashActivity.kt
+++ app/src/main/java/dev/w1zzrd/btpw/SplashActivity.kt
@@ -2,11 +2,12 @@ package dev.w1zzrd.btpw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window.FEATURE_NO_TITLE
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
